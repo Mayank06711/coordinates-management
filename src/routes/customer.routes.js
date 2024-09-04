483787952f26c005d2414149a28d2ab2a9f03965
Mayank Soni <mayank93soni@gmail.com>
2024-09-05 02:17:04 +0530

    add route for customer

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -3,11 +3,20 @@ import { upload } from "../middlewares/middleware.js";
 
 const router = new Router();
 
+// import controllers
+import { createCustomer } from "../controllers/customer.controller.js";
+
+
+// define routes
+
+router.route("/new").post(createCustomer);
 
 router.route("/uplo" ).post(upload,(req, res)=>{
     console.log(req.file);
     res.status(200).json({success: true});
 })
+
+
 
 
 export default router;
