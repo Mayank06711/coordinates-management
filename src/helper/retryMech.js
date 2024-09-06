9892570ef6b0f6055c501ea40122341460a44480
Mayank Soni <mayank93soni@gmail.com>
2024-09-06 22:32:44 +0530

    add helper funtion for retrying some operation which needs attention

---
+++ src/helper/retryMech.js
@@ -0,0 +1,17 @@
+import apiError from "../utils/apiError.js";
+
+const retryOperation = async (operation, retries = +process.env.MAX_RETRIES) => {
+    let attempt = 0;
+    while (attempt < retries) {
+      try {
+        return await operation(); // return whatever succeeded operation returns
+      } catch (error) {
+        attempt++;
+        if (attempt >= retries) throw new apiError(500,"operation failed even after maximum retries");
+        console.warn(`Retrying due to error: ${error.message}`);
+        await new Promise((resolve) => setTimeout(resolve, 2 ** attempt * 1000));//Exponential Backoff:
+      }
+    }
+  };
+
+export default retryOperation;
