41d9cc1f36fad2ccfacb6e651d2d0b6fdd72b14f
Mayank Soni <mayank93soni@gmail.com>
2024-09-06 21:39:51 +0530

    routes for aws service is done

--- src/routes/aws.routes.js
+++ src/routes/aws.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { upload } from "../middlewares/middleware.js";
-import {uploadOnS3} from "../controllers/aws.controller.js"
+import {uploadOnS3, getObjectS3, delObjectS3, getListObjectS3} from "../controllers/aws.controller.js"
 const router = new Router();
 
 router.route("/upload").post(upload,uploadOnS3);
-
-
+router.route("/object/:keyName").get(getObjectS3);
+router.route("/object/list").get(getListObjectS3);
+router.route("/object/del").delete(delObjectS3);
 export default router
